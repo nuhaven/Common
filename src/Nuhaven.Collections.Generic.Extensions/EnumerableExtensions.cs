2fed704da29568f6f16c29c0036748020373e01c
Guilherme Lepsch <lepsch@gmail.com>
2017-04-22 20:14:20 -0300

    Fix method documentation

--- src/Nuhaven.Collections.Generic.Extensions/EnumerableExtensions.cs
+++ src/Nuhaven.Collections.Generic.Extensions/EnumerableExtensions.cs
@@ -9,7 +9,6 @@ namespace Nuhaven.Collections.Generic
         /// <summary>
         /// Tells if a Enumerable is null or empty
         /// </summary>
-        /// </remarks>
         /// <param name="value">The IEnumerable instance</param>
         /// <returns>Returns true if the enumerable is empty or null</returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> value) =>
