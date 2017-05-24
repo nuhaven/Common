c369a2c10a87fce49f4282f2b278329608cc36d3
Guilherme Lepsch <lepsch@gmail.com>
2017-05-24 18:22:12 -0300

    Add StyleCop analyser

--- src/Nuhaven.Collections.Generic.Extensions/DictionaryExtensions.cs
+++ src/Nuhaven.Collections.Generic.Extensions/DictionaryExtensions.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace Nuhaven.Collections.Generic
 {
+    /// <summary>
+    /// Extensions for the IDictionary
+    /// </summary>
     public static class DictionaryExtensions
     {
         /// <summary>
@@ -11,6 +14,8 @@ namespace Nuhaven.Collections.Generic
         /// </summary>
         /// <param name="dict">The IDictionary instance</param>
         /// <param name="kvp">A key-value-pair to add</param>
+        /// <typeparam name="K">The dictionary key type</typeparam>
+        /// <typeparam name="V">The dictionary value type</typeparam>
         /// <exception cref="T:System.ArgumentNullException">key is null</exception>
         /// <exception cref="T:System.ArgumentException">
         /// An element with the same key already exists in the IDictionary object
--- src/Nuhaven.Collections.Generic.Extensions/EnumerableExtensions.cs
+++ src/Nuhaven.Collections.Generic.Extensions/EnumerableExtensions.cs
@@ -4,11 +4,15 @@ using System.Linq;
 
 namespace Nuhaven.Collections.Generic
 {
+    /// <summary>
+    /// Extensions for the IEnumerable
+    /// </summary>
     public static class EnumerableExtensions
     {
         /// <summary>
         /// Tells if a Enumerable is null or empty
         /// </summary>
+        /// <typeparam name="T">The value type of enumerable</typeparam>
         /// <param name="value">The IEnumerable instance</param>
         /// <returns>Returns true if the enumerable is empty or null</returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> value) =>
